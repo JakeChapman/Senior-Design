dc09ce253bd1fa202fb988f38bc169bb6c12e401
Jacob Chapman <chapmajb@mail.uc.edu>
2015-10-23 10:46:16 -0400

    Modified Navbar, added skeleton for pages and created routes for each

---
+++ client/javascript/layout.js
@@ -0,0 +1,7 @@
+if(Meteor.isClient){
+  Meteor.startup(function(){
+    var height = 20;
+    console.log(height);
+    //$('#layout-main-content').css('background-color','black');
+  });
+}
--- client/javascript/routes.js
+++ client/javascript/routes.js
@@ -5,4 +5,22 @@ Router.route('/', function() {
   this.render('landing');
   //Code used to specify which if any layout to be used
   this.layout('layout');
+  },
+  {
+    name : 'landing'
+});
+
+Router.route('/quiz',function(){
+  this.render('quiz');
+  this.layout('layout');
+});
+
+Router.route('/notes',function(){
+  this.render('notes');
+  this.layout('layout');
+});
+
+Router.route('/settings',function(){
+  this.render('settings');
+  this.layout('layout');
 });
