bc4e5a597be291317b4e54afe5dce2118ef3101a
bosticsa <bosticsa@mail.uc.edu>
2015-10-30 10:40:45 -0400

    Just trying to create the QuizPage branch. Very simple layout of quiz page and settings page.

---
+++ client/javascript/quiz.js
@@ -0,0 +1,12 @@
+
+Template.quiz.helpers({
+question : function() {
+  // This will eventually grab the questions from what the user inputs
+  //for now just return some simple stuff.
+  return 'This is a test question.';
+}
+});
+
+Template.quiz.events({
+
+});
