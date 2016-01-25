a262b7b731b2e0ed95f42e5a3f2db63f9bfff7a5
Jacob Chapman <chapmajb@mail.uc.edu>
2016-01-24 22:01:36 -0500

    Settings started, Jacob TODO: Finish reactive data, Sean TODO: verify bootstrap compnents work on multiple platforms

---
+++ client/QuestionSettings.jsx
@@ -0,0 +1,73 @@
+QuestionsSettings = React.createClass({
+  getInitialState() {
+    return {qpp: 0, startDate: "", endDate: "", startTime: "", endTime: ""}
+  },
+
+  render() {
+    return (
+      <form className="form-horizontal" role="form">
+
+        <div className="form-group" id="qSettings">
+          <label className="control-label col-sm-2 zmdi zmdi-help" for="questions-per-period">
+            Number of Questions per Period:</label>
+          <div className="col-sm-10">
+            <input className="form-control" name="questions-per-period" type="numeric"/>
+          </div>
+        </div>
+
+        <div className="datetimepicker form-group" id="period-settings">
+          <label className="control-label col-sm-2" for="start-period">
+            <i className="zmdi zmdi-calendar zmdi-hc"></i>
+            Start Date:</label>
+          <div className="col-sm-10">
+            <input className="form-control" id="start" name="start-period" type="date"/>
+          </div>
+        </div>
+
+        <div className="datetimepicker form-group" id="period-settings">
+          <label className="control-label col-sm-2" for="end-period">
+            <i className="zmdi zmdi-calendar zmdi-hc"></i>
+            End Date:</label>
+          <div className="col-sm-10">
+            <input className="form-control" id="end" name="end-period" type="date"/>
+          </div>
+        </div>
+
+        <div className="form-group" id="period-settings">
+          <label className="control-label col-sm-2" for="start-time">
+            <i className="zmdi zmdi-time zmdi-hc"></i>
+            Start Time:</label>
+          <div className="col-sm-10">
+            <input className="form-control" id="start-time" name="start-time" type="time"/>
+          </div>
+        </div>
+
+        <div className="form-group" id="period-settings">
+          <label className="control-label col-sm-2" for="end-time">
+            <i className="zmdi zmdi-time zmdi-hc"></i>
+            End Time:</label>
+          <div className="col-sm-10">
+            <input className="form-control" id="end-time" name="end-time" type="time"/>
+          </div>
+        </div>
+
+        <div className="form-group" id="totals">
+          <div className="col-sm-6">
+            <label className="control-label">
+              Total time of Period: Days:
+            </label>
+            <span className=" label label-default">{this.state.qpp}</span>
+          </div>
+          Number of Hours per Day:
+
+          <br/>
+          Number of Questions per Hour:
+
+          <br/>
+          Number of Questions per Day:
+
+        </div>
+      </form>
+    )
+  }
+});
--- client/UserSettings.jsx
+++ client/UserSettings.jsx
@@ -12,58 +12,8 @@ Settings = React.createClass({
     return(
       <div className="container" id="settings-wrapper">
 
-        <div id="question-configuration">
-          <h3>Questions Configuration</h3>
-          <div id="qpp-settings">
-            <label for="questions-per-period" id="QPP">
-              <i className="zmdi zmdi-help zmdi-hc"></i>
-              Number of Questions per Period:</label>
-            <input id="QPP-value" name="questions-per-period" type="numeric"/>
-          </div>
-
-          <div className="datetimepicker" id="period-settings">
-            <label for="start-period">
-              <i className="zmdi zmdi-calendar zmdi-hc"></i>
-              Start Date:</label>
-            <input id="start" name="start-period" type="date"/>
-          </div>
-
-          <div className="datetimepicker" id="period-settings">
-            <label for="end-period">
-              <i className="zmdi zmdi-calendar zmdi-hc"></i>
-              End Date:</label>
-            <input id="end" name="end-period" type="date"/>
-          </div>
-
-          <div id="period-settings">
-            <label for="start-time">
-              <i className="zmdi zmdi-time zmdi-hc"></i>
-              Start Time:</label>
-            <input id="start-time" name="start-time" type="time"/>
-          </div>
-
-          <div id="period-settings">
-            <label for="end-time">
-              <i className="zmdi zmdi-time zmdi-hc"></i>
-              End Time:</label>
-            <input id="end-time" name="end-time" type="time"/>
-          </div>
-
-          <div id="totals">
-            Total time of Period:
-
-            Days
-            <br/>
-            Number of Hours per Day:
-
-            <br/>
-            Number of Questions per Hour:
-
-            <br/>
-            Number of Questions per Day:
-
-          </div>
-        </div>
+          <h4 id="settingsHeader">Questions Configuration</h4>
+          <QuestionsSettings/>
 
         <div id="question-type-options">
           <h3>Type of Questions
