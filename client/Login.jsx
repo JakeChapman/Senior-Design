95885d7e96222365898902c8133f6905309184ac
Jacob Chapman <chapmajb@mail.uc.edu>
2016-02-03 08:32:03 -0500

    Fixed login styling

--- client/Login.jsx
+++ client/Login.jsx
@@ -31,35 +31,33 @@ Login = React.createClass({
   render() {
 
     return (
-      <div className="container center_div">
+      <div className="container center_div" id="login-wrapper">
         <form className="form-horizontal" role="form" onSubmit={this.handleLogin}>
-
-          <div className="form-group">
-            <label className="control-label col-md-4">
-              User Name
-            </label>
-            <div className="col-md-6">
-              <input className="form-control" id="userEmail" type="email"/>
+          <div className="col-md-6 col-md-offset-3" id="login-form">
+            <div className="form-group">
+              <label className="control-label col-md-4">
+                User Name
+              </label>
+              <div className="col-md-6">
+                <input className="form-control" id="userEmail" type="email"/>
+              </div>
             </div>
-          </div>
 
-          <div className="form-group">
-            <label className="control-label col-md-4">
-              Password
-            </label>
-            <div className="col-md-6">
-              <input className="form-control" id="userPassword" type="password"/>
+            <div className="form-group">
+              <label className="control-label col-md-4">
+                Password
+              </label>
+              <div className="col-md-6">
+                <input className="form-control" id="userPassword" type="password"/>
+              </div>
             </div>
-          </div>
 
-          <div className="form-group">
-            <div className="col-md-offset-4 col-md-10">
-              <button type="submit" className="btn btn-default">Submit</button>
+            <div className="form-group col-md-12">
+              <button type="submit" className="btn btn-default col-md-4 col-md-offset-4">Submit</button>
             </div>
+
+            <Registration/>
           </div>
-
-          <Registration/>
-
         </form>
       </div>
     )
