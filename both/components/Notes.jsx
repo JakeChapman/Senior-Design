7877c9c6909b32ee7c47192ce6f974925f008b62
Sean Bostic <Sean@seans-mbp.lang.uc.edu>
2016-03-02 16:46:27 -0500

    Parse branch with Papa parse

--- both/components/Notes.jsx
+++ both/components/Notes.jsx
@@ -8,19 +8,28 @@ Notes = React.createClass({
     $("#navNotes").toggleClass("nonfocus");
     $("#navNotes").toggleClass("focus");
   },
+
+    performParse(evt){
+        console.log("Trying  to import here");
+        var file = evt.target.files[0];
+        var data = "";
+        var test = Papa.parse(file, {
+            delimiter: "|",
+            skipEmptyLines: true,
+            complete: function(results){
+                console.log(results.data);
+            }
+            });
+        console.log(test);
+        //var attachment = $("#importBtn").filename;
+        //var parsedAttachment = Papa.parse(attachment, config);
+        //$("#file-text").value = parsedAttachment;
+    },
+
   render(){
     return(
       <div className="container" id="notes-container">
-
-        <div className="fileUpload btn btn-primary notes-import myFileInput" id="importBtn">
-            <span>Upload Notes</span>
-            <input id="uploadBtn" type="file" className="upload" />
-        </div>
-
-        <div className="notes-section">
-          <p id="file-text"></p>
-        </div>
-
+          <input type = "file" id="csv-file" name="file" onChange={this.performParse}/>
       </div>
     )
   }
